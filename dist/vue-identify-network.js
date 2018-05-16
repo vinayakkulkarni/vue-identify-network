482bc2a606421060822a19dc67c1392a9877c8ab
Vinayak <inbox.vinayak@gmail.com>
2018-05-17 03:01:47 +0530

    Let there be `dist`

---
+++ dist/vue-identify-network.js
@@ -0,0 +1,139 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.VueIdentifyNetwork = factory());
+}(this, (function () { 'use strict';
+
+  var script = {
+    name: 'VueIdentifyNetwork',
+    props: {
+      slowClass: {
+        type: String,
+        required: false
+      },
+      fastClass: {
+        type: String,
+        required: false
+      }
+    },
+    data: function data() {
+      return {
+        type: typeof window === 'undefined' ? 'Unknown' : navigator.connection.effectiveType
+      };
+    },
+    mounted: function mounted() {
+      window.addEventListener('load', function () {
+        this.identifyConnection();
+      });
+    },
+
+    methods: {
+      identifyConnection: function identifyConnection() {
+        this.type = navigator.connection.effectiveType || 'Unknown';
+        this.$emit('identified-network', this.type);
+      }
+    },
+    render: function render() {
+      return this.type;
+    }
+  };
+
+  var __vue_script__ = script;
+
+  var __vue_render__ = function __vue_render__() {
+    var _vm = this;
+    var _h = _vm.$createElement;
+    var _c = _vm._self._c || _h;
+    return _c("div", [_vm.type === "2g" ? _c("div", { class: _vm.slowClass }, [_vm._t("slow")], 2) : _vm._e(), _vm._v(" "), _vm.type !== "2g" ? _c("div", { class: _vm.fastClass }, [_vm._t("fast")], 2) : _vm._e()]);
+  };
+  var __vue_staticRenderFns__ = [];
+  __vue_render__._withStripped = true;
+
+  var __vue_template__ = typeof __vue_render__ !== 'undefined' ? { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ } : {};
+
+  var __vue_inject_styles__ = undefined;
+
+  var __vue_scope_id__ = undefined;
+
+  var __vue_module_identifier__ = undefined;
+
+  var __vue_is_functional_template__ = false;
+
+  function __vue_normalize__(template, style, script$$1, scope, functional, moduleIdentifier, createInjector, createInjectorSSR) {
+    var component = script$$1 || {};
+
+    {
+      component.__file = "/Users/vinayak/Development/Personal/vue-identify-network/src/VueIdentifyNetwork.vue";
+    }
+
+    if (!component.render) {
+      component.render = template.render;
+      component.staticRenderFns = template.staticRenderFns;
+      component._compiled = true;
+
+      if (functional) component.functional = true;
+    }
+
+    component._scopeId = scope;
+
+    return component;
+  }
+
+  function __vue_create_injector__() {
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
+    var isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+
+    return function addStyle(id, css) {
+      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) return;
+
+      var group = isOldIE ? css.media || 'default' : id;
+      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
+
+      if (!style.ids.includes(id)) {
+        var code = css.source;
+        var index = style.ids.length;
+
+        style.ids.push(id);
+
+        if (isOldIE) {
+          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
+        }
+
+        if (!style.element) {
+          var el = style.element = document.createElement('style');
+          el.type = 'text/css';
+
+          if (css.media) el.setAttribute('media', css.media);
+          if (isOldIE) {
+            el.setAttribute('data-group', group);
+            el.setAttribute('data-next-index', '0');
+          }
+
+          head.appendChild(el);
+        }
+
+        if (isOldIE) {
+          index = parseInt(style.element.getAttribute('data-next-index'));
+          style.element.setAttribute('data-next-index', index + 1);
+        }
+
+        if (style.element.styleSheet) {
+          style.parts.push(code);
+          style.element.styleSheet.cssText = style.parts.filter(Boolean).join('\n');
+        } else {
+          var textNode = document.createTextNode(code);
+          var nodes = style.element.childNodes;
+          if (nodes[index]) style.element.removeChild(nodes[index]);
+          if (nodes.length) style.element.insertBefore(textNode, nodes[index]);else style.element.appendChild(textNode);
+        }
+      }
+    };
+  }
+
+
+  var VueIdentifyNetwork = __vue_normalize__(__vue_template__, __vue_inject_styles__, typeof __vue_script__ === 'undefined' ? {} : __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, typeof __vue_create_injector__ !== 'undefined' ? __vue_create_injector__ : function () {}, typeof __vue_create_injector_ssr__ !== 'undefined' ? __vue_create_injector_ssr__ : function () {});
+
+  return VueIdentifyNetwork;
+
+})));
