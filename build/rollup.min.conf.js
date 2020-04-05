4e4138ac21f2d78ecbfc5b91c5da61b490b510dc
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-04-05 20:55:47 +0530

    feat: major housekeeping

--- build/rollup.min.conf.js
+++ build/rollup.min.conf.js
@@ -1,11 +1,11 @@
+import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 import vue from 'rollup-plugin-vue';
-import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.js',
-  plugins: [commonjs(), vue(), babel(), uglify()],
+  plugins: [commonjs(), babel(), vue(), terser()],
   output: {
     format: 'umd',
     name: 'VueIdentifyNetwork',
--- src/VueIdentifyNetwork.vue
+++ src/VueIdentifyNetwork.vue
@@ -1,63 +1,69 @@
 <template>
   <div>
-    <div :class="unknownClass" v-if="type === 'Unknown'">
+    <div v-if="type === 'Unknown'" :class="unknownClass">
       <slot name="unknown"></slot>
     </div>
-    <div :class="slowClass" v-if="type === '2g' && type !== 'Unknown'">
+    <div v-if="type === '2g' && type !== 'Unknown'" :class="slowClass">
       <slot name="slow"></slot>
     </div>
-    <div :class="fastClass" v-if="type !== '2g' && type !== 'Unknown'">
+    <div v-if="type !== '2g' && type !== 'Unknown'" :class="fastClass">
       <slot name="fast"></slot>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'VueIdentifyNetwork',
-  props: {
-    unknownClass: {
-      type: String,
-      required: false,
+  export default {
+    name: 'VueIdentifyNetwork',
+    props: {
+      unknownClass: {
+        type: String,
+        required: false,
+        default: null,
+      },
+      slowClass: {
+        type: String,
+        required: false,
+        default: null,
+      },
+      fastClass: {
+        type: String,
+        required: false,
+        default: null,
+      },
     },
-    slowClass: {
-      type: String,
-      required: false,
-    },
-    fastClass: {
-      type: String,
-      required: false,
-    },
-  },
-  data: () => ({
-    type: null,
-    downLink: null,
-    vendor: typeof window === 'undefined' ? 'Unknown' : navigator.vendor,
-  }),
-  mounted() {
-    const t = this;
-    if (t.vendor.includes('Google') && t.type !== 'Unknown') {
-      t.type = navigator.connection.effectiveType;
-      t.downLink = navigator.connection.downlink;
-    } else {
-      t.type = 'Unknown';
-      t.downLink = 'Unknown';
-    }
-    t.$emit('network-type', t.type);
-    t.$emit('network-speed', t.downLink);
-    navigator.connection.addEventListener('change', t.updateConnectionMeta);
-  },
-  beforeDestroy() {
-    navigator.connection.removeEventListener('change', this.updateConnectionMeta);
-  },
-  methods: {
-    updateConnectionMeta(e) {
+    data: () => ({
+      type: null,
+      downLink: null,
+      vendor: typeof window === 'undefined' ? 'Unknown' : navigator.vendor,
+    }),
+    mounted() {
       const t = this;
-      t.type = e.currentTarget && e.currentTarget.effectiveType;
-      t.downLink = e.currentTarget && e.currentTarget.downlink;
+      if (t.vendor.includes('Google') && t.type !== 'Unknown') {
+        t.type = navigator.connection.effectiveType;
+        t.downLink = navigator.connection.downlink;
+      } else {
+        t.type = 'Unknown';
+        t.downLink = 'Unknown';
+      }
       t.$emit('network-type', t.type);
       t.$emit('network-speed', t.downLink);
+      navigator.connection.addEventListener('change', t.updateConnectionMeta);
     },
-  },
-};
+    beforeDestroy() {
+      navigator.connection.removeEventListener(
+        'change',
+        this.updateConnectionMeta,
+      );
+    },
+    methods: {
+      updateConnectionMeta(e) {
+        const t = this;
+        t.type = e.currentTarget && e.currentTarget.effectiveType;
+        t.downLink = e.currentTarget && e.currentTarget.downlink;
+        t.$emit('network-type', t.type);
+        t.$emit('network-speed', t.downLink);
+      },
+    },
+  };
 </script>
