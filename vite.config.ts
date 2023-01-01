38cdeafab5c970a025af4baa22698b6e1de01280
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-01-02 03:36:33 +0530

    chore: minor housekeeping
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+
+const banner = `/*!
+* ${pkg.name} v${pkg.version}
+* (c) ${new Date().getFullYear()} ${pkg.author.name}
+* @license ${pkg.license}
+*/`;
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueIdentifyNetwork',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: pkg.name,
+    },
+    commonjsOptions: {
+      extensions: ['.js', '.ts', '.vue'],
+      exclude: 'src/**',
+      include: 'node_modules/**',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        banner,
+        strict: true,
+        sourcemap: true,
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'vue',
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'vue-identify-network.css';
+          return assetInfo.name;
+        },
+      },
+    },
+  },
+});
