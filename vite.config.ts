8532dead34948c38a9edc1c10188abd4a5c51bf4
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-08 04:15:14 +0530

    refactor: update vite.config.ts for ESM-only output

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,22 @@
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import pkg from './package.json';
-
-const banner = `/*!
- * ${pkg.name} v${pkg.version}
- * ${pkg.description}
- * (c) ${new Date().getFullYear()} ${pkg.author.name}<${pkg.author.email}>
- * Released under the ${pkg.license} License
- */
-`;
 
 export default defineConfig({
+  plugins: [vue()],
   build: {
     target: 'esnext',
     sourcemap: true,
-    reportCompressedSize: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VueIdentifyNetwork',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: pkg.name,
-    },
-    commonjsOptions: {
-      extensions: ['.js', '.ts', '.vue'],
-      strictRequires: true,
-      transformMixedEsModules: true,
-      exclude: 'src/**',
-      include: 'node_modules/**',
+      formats: ['es'],
+      fileName: 'vue-identify-network',
     },
     rollupOptions: {
       external: ['vue'],
       output: {
-        banner,
         exports: 'named',
-        strict: true,
-        extend: true,
-        globals: {
-          vue: 'vue',
-        },
       },
     },
   },
-  plugins: [vue()],
 });
