ec53e347dd7ac0693ef7343112b2f6793ba8d9aa
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-05-19 16:37:40 +0530

    chore(example): migrate to tailwindcss v4
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import UnoCSS from 'unocss/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue(), tailwindcss()],
 });
